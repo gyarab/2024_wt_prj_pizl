da677c1fefecc1405aa70c062393c021ee565dd6
Josef Pizl <josef.pizl.s@gyrab.cz>
2025-02-28 08:49:06 +0100

    krystofuv kod jen rikam cuz not moje

--- exchange.py
+++ exchange.py
@@ -1,22 +1,65 @@
-import httpx 
+import httpx
 
+url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=13.02.2025"
+r = httpx.get(url)
 
-print("online prevodnik neceho idk")
+# print(r.text)
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=13.02.2025')
+lines = r.text.split("\n")
 
-##print(r.text)
-
-in_array = r.text.split("\n")
-
-for line in in_array:
+row = ""
+for line in lines:
     if "|EUR|" in line:
-        print(line)
+        row = line
+        
+# print(row)
+
+row_arr = row.split("|")
+
+kurz_str = row_arr[-1]
+kurz_str = kurz_str.replace(",", ".")
+
+kurz = float(kurz_str)
+# print(kurz)
+
+while(1):
+    mode = input("Zadej mód převodu: 1 = CZK na EUR, 2 = EUR na CZK\n")
+    
+    if mode == '1':
+        currency = "CZK"
+        break
+    
+    elif mode == '2':
+        currency = "EUR"
+        break
+    else:
+        print("Nepovolená hodnota, prosím zadejte jinou.")
+        mode = 0
+        
+
+while(1):
+    value_str = input("Zadej hodnotu " + currency + " pro převedení: \n")
+    
+    try:
+        value = float(value_str)
+        break
+    except:
+        print("Nepovolená hodnota, prosím zadejte jinou.")
+        value = 0    
 
 
-
-
-castka = input("zadej castku v czk: ")
-
-result = int(castka) * 25
-print(f"to je v EUR: {result}")
+while(1):
+           
+    
+    if mode == '1':
+        result = value / kurz
+        print(f"Výsledek je {result} EUR")
+        break
+    elif mode == '2':
+        result = value * kurz
+        print(f"Výsledek je {result} CZK")
+        break
+    else:
+        print("Nepovolená hodnota, prosím zadejte jinou.")
+        value = 0
+        break
