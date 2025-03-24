eabaa892d6070a8f43a3201ff254538a8879a9a1
Josef Pizl <josef.pizl.s@gyrab.cz>
2025-03-24 14:53:32 +0100

    ok stalo se neco idk vincent helped me so he should be blamed for everzthing

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 from .models import Pecivo
+
+class PecivoAdmin(admin.ModelAdmin):
+    list_display = ["jmeno","popis","cena"]
+
+admin.site.register(Pecivo, PecivoAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 class Pecivo(models.Model):
-    name = models.CharField(max_length=300)
-    year = models.IntegerField(blank=False,null=True)
+    jmeno = models.CharField(max_length=50)
+    popis = models.CharField(max_length=300)
+    cena = models.IntegerField(null=True, blank = True)
+
+    def __str__(self):
+        return f"{self.name}"
