a29e8223455975efd371dc2028722187a0bac5a4
Josef Pizl <josef.pizl.s@gyrab.cz>
2025-05-02 10:15:44 +0200

    ok neco se stalo celkem confuzed

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Pecivo
 
-# Create your views here.
+def get_homepage(request):
+    context = {
+        "svatek": "Marek",
+        "pecivo": Pecivo.objects.all().order_by('jmeno') [:10]
+    }
+
+    print("HOST",request.get_host())
+    
+    if(request.GET.get("search")):
+        print("SEARCH",request.GET.get("search"))
+        pecivo = pecivo.filtet(title__contains=request.GET.get("search"))
+
+
+    return render(
+        request, "main/homepage.html", context
+        )
