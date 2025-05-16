00c1b14b174cb9089a66d53c000497c7a060ee34
Josef Pizl <josef.pizl.s@gyrab.cz>
2025-05-16 11:39:41 +0200

    fungujeeeeeeeee tooooooooooooooooooooooooooooooooo

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from .models import Pecivo
+from .models import Pecivo, Kategorie
 
 def get_homepage(request):
-    pecivos = Pecivo.objects.prefetch_related('pecivokategorie_set__kategorie').all()
+    pecivos = Pecivo.objects.select_related('kategorie').all()
+    kategorie = Kategorie.objects.all()
 
     if request.GET.get("search"):
         search_term = request.GET.get("search")
-        print("SEARCH", search_term)
-        pecivos = Pecivo.objects.filter(jmeno__icontains=search_term)
+        pecivos = pecivos.filter(jmeno__icontains=search_term)
+
+    if request.GET.get("category"):
+        category_id = request.GET.get("category")
+        pecivos = pecivos.filter(kategorie__id=category_id)
 
     context = {
         "svatek": "Marek",
-        "pecivos": pecivos
+        "pecivos": pecivos,
+        "kategorie": kategorie,
     }
 
     return render(request, "main/homepage.html", context)
